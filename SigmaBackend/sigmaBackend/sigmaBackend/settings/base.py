df884ab20f1adf73dd0cbf86dbca351c39f1046a
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-07-15 18:39:21 +0200

    change for collectstatic

--- SigmaBackend/sigmaBackend/sigmaBackend/settings/base.py
+++ SigmaBackend/sigmaBackend/sigmaBackend/settings/base.py
@@ -125,6 +125,8 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / "media"
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
