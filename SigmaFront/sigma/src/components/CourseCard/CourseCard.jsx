7558b555c9b1d0d7334af85cb890e3373cc9fc1d
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-07-28 23:29:31 +0200

    htmlParserAddedFix2

--- SigmaFront/sigma/src/components/CourseCard/CourseCard.jsx
+++ SigmaFront/sigma/src/components/CourseCard/CourseCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./CourseCard.css" 
 import { Link } from 'react-router-dom';
-
+import parse from 'html-react-parser';
 
 
 const CourseCard = ({item}) => {
@@ -17,7 +17,7 @@ const CourseCard = ({item}) => {
         <div className='CourseCard__container-info_container'>
             <div>
                 <h3>{item.title} <span>({item.course_type})</span></h3>
-                <p>{item.description.slice(0,120) + "..."}</p> 
+                <p>{parse(String(item.description.slice(0,120) + "..."))}</p> 
             </div>
            
             <Link to={`/kurs/${item.id}`} className='CourseCard__container-info_container-button'><button className='CourseCard__container-info_container-link'>Detaljnije</button></Link>
