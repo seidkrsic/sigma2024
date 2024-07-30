bb8033cd9e69f9bb793c293d7db46cc8da3244de
Seid  Krsic <seidkrsic@Seids-Air.home>
2024-07-30 16:58:32 +0200

    VukpredloziFix15

--- SigmaBackend/sigmaBackend/courses_app/admin.py
+++ SigmaBackend/sigmaBackend/courses_app/admin.py
@@ -1,23 +1,23 @@
 from django.contrib import admin
-# from .models import Course
+from .models import Course
 from django_ckeditor_5.widgets import CKEditor5Widget
 from django import forms 
 
 
-# class CourseAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Course
-#         fields = '__all__'
-#         widgets = {
-#                     "description": CKEditor5Widget(
-#                         attrs={"class": "django_ckeditor_5"}, config_name="extends"
-#                     )
-#                 } 
+class CourseAdminForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = '__all__'
+        widgets = {
+                    "description": CKEditor5Widget(
+                        attrs={"class": "django_ckeditor_5"}, config_name="extends"
+                    )
+                } 
 
 
-# @admin.register(Course)
-# class CourseAdmin(admin.ModelAdmin):
-#     list_display = ('title','price', 'is_active')
-#     search_fields = ('title','description')
-#     list_filter = ('is_active',) 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title','price', 'is_active')
+    search_fields = ('title','description')
+    list_filter = ('is_active',) 
   
