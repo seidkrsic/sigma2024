c7df2924c1d8b8dfe0a208d6e49c98ee16c0b314
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-07-31 13:03:34 +0200

    FixingMigrations2

--- SigmaBackend/sigmaBackend/courses_app/serializers.py
+++ SigmaBackend/sigmaBackend/courses_app/serializers.py
@@ -1,25 +1,25 @@
 
 
-from rest_framework import serializers
-from .models import Course 
-from user_app.serializers import ProfileSerializer 
-from django.conf import settings
+# from rest_framework import serializers
+# from .models import Course 
+# from user_app.serializers import ProfileSerializer 
+# from django.conf import settings
 
-class CourseSerializer(serializers.ModelSerializer):
-    instructor = ProfileSerializer(many=True)
-    image_url = serializers.SerializerMethodField() 
-    price_per_hour = serializers.SerializerMethodField() 
-    class Meta:
-        model = Course
-        fields = '__all__'  
-        read_only_fields = ['created_at', 'updated_at']  
+# class CourseSerializer(serializers.ModelSerializer):
+#     instructor = ProfileSerializer(many=True)
+#     image_url = serializers.SerializerMethodField() 
+#     price_per_hour = serializers.SerializerMethodField() 
+#     class Meta:
+#         model = Course
+#         fields = '__all__'  
+#         read_only_fields = ['created_at', 'updated_at']  
 
-    def get_image_url(self, obj):
-        if obj.image:
-            return f"{settings.BASE_URL}{obj.image.url}"
-        return None
+#     def get_image_url(self, obj):
+#         if obj.image:
+#             return f"{settings.BASE_URL}{obj.image.url}"
+#         return None
 
-    def get_price_per_hour(self, obj): 
-        if obj.price: 
-            return f"7.50" 
-        return None 
+#     def get_price_per_hour(self, obj): 
+#         if obj.price: 
+#             return f"7.50" 
+#         return None 
