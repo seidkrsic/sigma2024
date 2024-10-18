e3dc8fd76db8f5d77fdb4f0470b63ff23d22c214
Seid  Krsic <seidkrsic@Seids-Air.home>
2024-10-18 19:58:12 +0200

    NoviLoginRegisterSistemFix2

--- SigmaBackend/sigmaBackend/sigmaBackend/settings/base.py
+++ SigmaBackend/sigmaBackend/sigmaBackend/settings/base.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import os
 import environ 
 
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
