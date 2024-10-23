578531c7f4025ef7a7cec84e46ad469db6464694
Seid  Krsic <seidkrsic@Seids-Air.home>
2024-10-23 20:00:18 +0200

    BlogFix4

--- SigmaFront/sigma/src/components/SideMenu/SideMenu.jsx
+++ SigmaFront/sigma/src/components/SideMenu/SideMenu.jsx
@@ -2,23 +2,51 @@
 import React from 'react';
 import './SideMenu.css';
 import { Link } from 'react-router-dom';
-import sigma2 from "../../images/sigma_2.png" 
+import sigma2 from "../../images/sigma_2.png"; 
 
 const SideMenu = ({ isOpen, toggleMenu }) => {
   return (
     <div className={`side-menu ${isOpen ? 'open' : ''}`}>
       <ul>
-       
-        <Link id="special__link" to="/informacije" onClick={toggleMenu}>Informacije</Link>
-       
+        <li>
+          <Link id="special__link" to="/informacije" onClick={toggleMenu}>
+            Informacije
+          </Link>
+        </li>
         <div className='tapered-line'></div>
-        
-        <Link id="special__link" to="/kursevi" onClick={toggleMenu}>Kursevi</Link>
-        
-        <div className='tapered-line'></div> 
-        
-        <img className='side-menu__img' src={sigma2} alt="" />
-        <p id='side-menu__text'>Sigma Acamedy</p>
+
+        <li>
+          <Link id="special__link" to="/kursevi" onClick={toggleMenu}>
+            Kursevi
+          </Link>
+        </li>
+        <div className='tapered-line'></div>
+
+        <li>
+          <Link id="special__link" to="/problem-of-the-week" onClick={toggleMenu}>
+            Problem of the Week
+          </Link>
+        </li>
+        <div className='tapered-line'></div>
+
+        <li>
+          <Link id="special__link" to="/problems" onClick={toggleMenu}>
+            Arhiva problema
+          </Link>
+        </li>
+        <div className='tapered-line'></div>
+
+        <li>
+          <Link id="special__link" to="/blog" onClick={toggleMenu}>
+            Blog
+          </Link>
+        </li>
+        <div className='tapered-line'></div>
+
+        <li>
+          <img className='side-menu__img' src={sigma2} alt="Sigma Academy Logo" />
+          <p id='side-menu__text'>Sigma Academy</p>
+        </li>
       </ul>
     </div>
   );
