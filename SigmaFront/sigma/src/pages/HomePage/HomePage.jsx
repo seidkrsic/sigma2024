67f2c28ee6bb641c259972582b7f36861ee2b1e3
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-10-24 23:30:43 +0200

    HomePageStyleFix3

--- SigmaFront/sigma/src/pages/HomePage/HomePage.jsx
+++ SigmaFront/sigma/src/pages/HomePage/HomePage.jsx
@@ -30,12 +30,12 @@ const HomePage = () => {
         </div> 
 
         
-        <WavyDiv title="Problem Nedjelje">
+      <WavyDiv title="Problem Nedjelje">
         <div className='Problem_of_the_Week__container'>
           <div className='Problem_of_the_Week__link-container'>
-            <img src={glavnaSlika} alt="Problem Nedjelje" />
-            <Link id="problem_of_the_week__problem-btn" to="/problem-of-the-week">Vidi Problem</Link>
-            <Link id='problem_of_the_week__archive-btn' to="/problems">Arhiva Problema</Link>
+            <img src={slika1} alt="Problem Nedjelje" />
+            <Link id="problem_of_the_week__problem-btn" to={"/problem-of-the-week"}>Vidi Problem</Link>
+            <Link id='problem_of_the_week__archive-btn' to={"/problems"}>Arhiva Problema</Link>
           </div>
           <div className='Problem_of_the_Week__link-container'>
             <p>
