858d0989b7c86f29b99ab89de3e1ccedf5baf5c8
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-10-31 16:28:19 +0100

    problemOftheWeekFix27

--- SigmaFront/sigma/src/pages/ProblemList/ProblemList.jsx
+++ SigmaFront/sigma/src/pages/ProblemList/ProblemList.jsx
@@ -9,7 +9,7 @@ const ProblemList = () => {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-
+  
   const pageSize = 10; // Veličina stranice
 
   const fetchProblems = (page = 1) => {
@@ -29,26 +29,57 @@ const ProblemList = () => {
         setLoading(false);
       });
 
-    window.scrollTo(0, 0);
-  };
+    document.querySelector(".scroll-container").scrollTo(0, 0);
+    setTimeout(() => {
+        document.querySelector(".scroll-container").scrollTo(0, 0);
+      }, 100);
 
+  };
+  
   useEffect(() => {
+   
     fetchProblems();
   }, []);
 
-  useScrollToTop();
+  useScrollToTop(); 
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
       fetchProblems(currentPage + 1);
     }
+    document.querySelector(".scroll-container").scrollTo(0, 0);
+    setTimeout(() => {
+        document.querySelector(".scroll-container").scrollTo(0, 0);
+      }, 100);
+  
   };
-
+  
   const handlePrevPage = () => {
     if (currentPage > 1) {
       fetchProblems(currentPage - 1);
     }
+    document.querySelector(".scroll-container").scrollTo(0, 0);
+    setTimeout(() => {
+        document.querySelector(".scroll-container").scrollTo(0, 0);
+      }, 100);
+
   };
+
+  const pagesToShow = [];
+
+  if (totalPages <= 3) {
+    for (let i = 1; i <= totalPages; i++) {
+      pagesToShow.push(i);
+    }
+  } else {
+    if (currentPage === 1) {
+      pagesToShow.push(1, 2, '...', totalPages);
+    } else if (currentPage === totalPages) {
+      pagesToShow.push(1, '...', totalPages - 1, totalPages);
+    } else {
+      pagesToShow.push(1, '...', currentPage, '...', totalPages);
+    }
+  }
 
   return (
     <div className='ProblemList__container'>
@@ -60,47 +91,59 @@ const ProblemList = () => {
         </div>
       ) : (
         <>
-          <div className="problem-tables">
-            {problems.map(problem => (
-              <table key={problem.id} className="problem-table">
-                <thead>
-                  <tr>
-                    <th>Datum objave</th>
-                    <th>Naziv</th>
-                    <th>Postavka</th>
-                    <th>Rješenje</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{new Date(problem.published_date).toLocaleDateString()}</td>
-                    <td>{problem.title}</td>
-                    <td>
-                      <a href={problem.problem_file_url} rel="noopener noreferrer" className="problem-link" target="_blank">
-                        Preuzmi problem
+          <table className="problem-list-table">
+            <thead>
+              <tr>
+                <th>Datum objave</th>
+                <th>Naziv</th>
+                <th>Postavka (PDF)</th>
+                <th>Rješenje (PDF)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {problems.map(problem => (
+                <tr key={problem.id}>
+                  <td data-label="Datum objave">{new Date(problem.published_date).toLocaleDateString()}</td>
+                  <td data-label="Naziv">{problem.title}</td>
+                  <td data-label="Postavka (PDF)">
+                    <a href={problem.problem_file_url} rel="noopener noreferrer" className="problem-link">
+                      Problem
+                    </a>
+                  </td>
+                  <td data-label="Rješenje (PDF)">
+                    {problem.solution_file_url ? (
+                      <a href={problem.solution_file_url} rel="noopener noreferrer" className="problem-link">
+                        Rješenje
                       </a>
-                    </td>
-                    <td>
-                      {problem.solution_file_url ? (
-                        <a href={problem.solution_file_url} rel="noopener noreferrer" className="problem-link" target="_blank">
-                          Preuzmi rješenje
-                        </a>
-                      ) : (
-                        <span className="no-solution">Nije dostupno</span>
-                      )}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            ))}
-          </div>
+                    ) : (
+                      <span className="no-solution">Nije dostupno</span>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
           <div className="pagination-buttons">
             {currentPage > 1 && (
               <button onClick={handlePrevPage} className="pagination-button">
                 Prethodna
               </button>
             )}
-            <span className="current-page">{currentPage} od {totalPages}</span>
+
+            {pagesToShow.map((page, index) => (
+              page === '...' ? (
+                <span key={index} className="pagination-dots">...</span>
+              ) : (
+                <button
+                  key={page}
+                  onClick={() => fetchProblems(page)}
+                  className={`pagination-button ${page === currentPage ? 'active' : ''}`}
+                >
+                  {page}
+                </button>
+              )
+            ))}
+
             {currentPage < totalPages && (
               <button onClick={handleNextPage} className="pagination-button">
                 Sledeća
