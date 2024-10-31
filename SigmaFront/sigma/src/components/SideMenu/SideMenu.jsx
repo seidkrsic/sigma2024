f3b6a1e5e3397b52de3a411182e8a778e05d0e7a
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-10-31 17:23:35 +0100

    problemOftheWeekFix32

--- SigmaFront/sigma/src/components/SideMenu/SideMenu.jsx
+++ SigmaFront/sigma/src/components/SideMenu/SideMenu.jsx
@@ -1,53 +1,42 @@
-// SideMenu.js
-import React from 'react';
+// SideMenu.jsx
+import React, { useEffect, useRef } from 'react';
 import './SideMenu.css';
 import { Link } from 'react-router-dom';
-import sigma2 from "../../images/sigma_2.png" 
-import { useEffect } from 'react';
+import sigma2 from "../../images/sigma_2.png";
 
 const SideMenu = ({ isOpen, toggleMenu }) => {
+  const sideMenuRef = useRef(null);
 
   useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add('menu-open');
-    } else {
-      document.body.classList.remove('menu-open');
-    }
-  }, [isOpen]);
-  
+    const handleClickOutside = (event) => {
+      if (isOpen && sideMenuRef.current && !sideMenuRef.current.contains(event.target)) {
+        toggleMenu();
+      }
+    };
 
-
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, toggleMenu]);
 
   return (
-    <div className={`side-menu ${isOpen ? 'open' : ''}`}>
+    <div ref={sideMenuRef} className={`side-menu ${isOpen ? 'open' : ''}`}>
       <ul>
-
-        {/* <div className='tapered-line'></div> */}
-        
         <Link id="special__link" to="/informacije" onClick={toggleMenu}>O nama</Link>
-       
         <div className='tapered-line'></div>
-        
-        {/* <Link id="special__link" to="/kursevi" onClick={toggleMenu}>Kursevi</Link>
-        
-        <div className='tapered-line'></div>   */} 
 
         <Link id="special__link" to="/posts" onClick={toggleMenu}>Blog</Link>
-        
-        <div className='tapered-line'></div> 
+        <div className='tapered-line'></div>
 
         <Link id="special__link" to="/problem-of-the-week" onClick={toggleMenu}>Problem nedjelje</Link>
-        
-        <div className='tapered-line'></div>  
+        <div className='tapered-line'></div>
 
         <Link id="special__link" to="/problems" onClick={toggleMenu}>Arhiva Problema</Link>
-        
-        <div className='tapered-line'></div> 
+        <div className='tapered-line'></div>
 
         <Link id="special__link" to="/rankings" onClick={toggleMenu}>Lista Najboljih</Link>
-        
-        {/* <div className='tapered-line'></div>  */}
-        
+
         <img className='side-menu__img' src={sigma2} alt="" />
         <p id='side-menu__text'>Sigma Academy</p>
       </ul>
