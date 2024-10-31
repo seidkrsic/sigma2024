963fd28622b8707e196e45284c8914ce74bf02b0
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-10-31 17:18:56 +0100

    problemOftheWeekFix31

--- SigmaFront/sigma/src/components/SideMenu/SideMenu.jsx
+++ SigmaFront/sigma/src/components/SideMenu/SideMenu.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './SideMenu.css';
 import { Link } from 'react-router-dom';
 import sigma2 from "../../images/sigma_2.png" 
+import { useEffect } from 'react';
 
 const SideMenu = ({ isOpen, toggleMenu }) => {
 
